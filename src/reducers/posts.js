e945ed8f43eefbc8127da313563f9d21b7d88219
Louisss-Lam <louislam617@gmail.com>
2022-11-03 18:20:44 +0000

    feat: add like post function

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -3,6 +3,7 @@ export default (posts = [], action) => {
         case 'DELETE':
             return posts.filter((post) => post._id != action.payload);
         case 'UPDATE':
+        case 'LIKE':
             return posts.map((post) => post._id === action.payload._id ? action.payload : post);
         case 'FETCH_ALL':
             return action.payload;
@@ -11,4 +12,4 @@ export default (posts = [], action) => {
         default:
             return posts;
     }
-}
+} 
