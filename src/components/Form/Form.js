ea800b1a343bf971f69336f5f15e2dd9cac0ec2c
Louisss-Lam <louislam617@gmail.com>
2022-11-04 17:39:14 +0000

    constant set up

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -67,7 +67,7 @@ const Form = ({ currentId, setCurrentId }) => {
                     variant="outlined" 
                     label="Tags" 
                     fullWidth value={postData.tags}
-                    onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+                    onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })}
                 />
                 <div className={classes.fileInput}>
                     <FileBase 
